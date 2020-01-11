14e7eedefe325bf95999b809080bbfe05bc2882f
Ganesh Shreedhar <Ganesh.Shreedhar@spartasystems.com>
2020-01-11 13:54:36 +0530

    adding theme to circularProgressBar

--- force-app/main/default/lwc/apiPropertiesMultiple/apiPropertiesMultiple.js
+++ force-app/main/default/lwc/apiPropertiesMultiple/apiPropertiesMultiple.js
@@ -1,7 +1,23 @@
 import { LightningElement, track } from 'lwc';
 
 export default class ApiPropertiesMultiple extends LightningElement {
-    @track percentage = 10;
+    @track 
+    percentage = 10;
+
+    @track
+    themeOptions = [
+        { value: 'blue', label: 'Blue' },
+        { value: 'green', label: 'Green' },
+        { value: 'orange', label: 'Orange' },
+        { value: 'red', label: 'Red' }
+    ];
+
+    @track
+    themeValue = 'blue';
+
+    handleThemeChange(event) {
+        this.themeValue = event.target.value;
+    }
 
     handlePercentageChange(event) {
         this.percentage = event.target.value;
